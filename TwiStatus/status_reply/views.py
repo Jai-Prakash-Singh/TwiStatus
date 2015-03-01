397fff0d4821aff2a18bce4a530f25b48fe307c8
Jai-Prakash-Singh <jp213@ymail.com>
2015-03-01 10:45:20 +0530

    v3

--- TwiStatus/status_reply/views.py
+++ TwiStatus/status_reply/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 from django.template import RequestContext
-
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 from forms import InputForm
-from twitter_status.settings import TWITTER_API
+from twitter_status.settings import TWITTER_API as api
 
 def get_status_reply(request):
-    
     template_name = "status_reply.html" 
 
     ci = RequestContext(request) 
@@ -19,11 +18,10 @@ def get_status_reply(request):
         if form.is_valid():
             data = form.cleaned_data['status_id']
             #date = 571542404156809216
-	    api = TWITTER_API
+            request.session["tweet_id"] = data
 
-	    main_status = api.GetStatus(id=data) 
-
-	    reply_status_old = api.GetReplies(since_id=data)
+            main_status = api.GetStatus(id=data) 
+            reply_status_old = api.GetReplies(since_id=data)
             reply_status = []
 
             for i in reply_status_old:
@@ -32,3 +30,22 @@ def get_status_reply(request):
             return render_to_response(template_name, {'form':form, "main_status":main_status, "reply_status":reply_status}, ci)
 
     return render_to_response(template_name, {'form':form}, ci)
+
+
+
+
+@csrf_exempt
+def ajax_get_reply(request):
+    ci = RequestContext(request)
+    template_name = "ajax_get_reply.html"
+    tweet_id = request.session.get("tweet_id")
+    reply_status_old = api.GetReplies(since_id=tweet_id)
+    reply_status = []
+
+    for i in reply_status_old:
+        reply_status.append(i.AsDict())
+
+        print reply_status
+
+    return render_to_response(template_name, {"reply_status":reply_status}, ci)
+    # return HttpResponse(tweet_id)
--- TwiStatus/twitter_status/urls.py
+++ TwiStatus/twitter_status/urls.py
@@ -5,7 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'status_reply.views.get_status_reply', name='status_reply'),
+    url(r'^$', 'status_reply.views.get_status_reply', name='get_status_reply'),
+    url(r'^ajax_get_reply/$', 'status_reply.views.ajax_get_reply', name='ajax_get_reply'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
    
